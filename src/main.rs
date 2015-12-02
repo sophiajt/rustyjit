566b33ff4efee70d9ead16570aaf5b72e5138262
Matt Ickstadt <matthew.ickstadt@gmail.com>
2015-12-02 02:15:17 -0600

    Make allocation work cross-platform.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,47 @@
 extern crate libc;
-extern crate winapi;
-extern crate kernel32;
 
 use std::mem;
 use std::ops::{Index, IndexMut};
 
 extern {
     fn memset(s: *mut libc::c_void, c: libc::uint32_t, n: libc::size_t) -> *mut libc::c_void;
-    fn _aligned_malloc(size: libc::size_t, alignment: libc::size_t) -> *mut libc::c_void;
+}
+
+#[cfg(windows)]
+mod memory {
+    extern crate libc;
+    extern crate winapi;
+    extern crate kernel32;
+
+    extern {
+        fn _aligned_malloc(size: libc::size_t, alignment: libc::size_t) -> *mut libc::c_void;
+    }
+
+    pub unsafe fn aligned_malloc(size: libc::size_t, alignment: libc::size_t) -> *mut libc::c_void {
+        _aligned_malloc(size, alignment)
+    }
+
+    pub unsafe fn make_executable(addr: *mut libc::c_void, size: libc::size_t) {
+        let mut _previous_protect : *mut u32 = &mut 0u32 as *mut u32;
+        kernel32::VirtualProtect(addr as *mut ::std::os::raw::c_void, size as u64, 
+            winapi::winnt::PAGE_EXECUTE_READWRITE, _previous_protect as u32);
+    }
+}
+
+#[cfg(unix)]
+mod memory {
+    extern crate libc;
+
+    pub unsafe fn aligned_malloc(size: libc::size_t, alignment: libc::size_t) -> *mut libc::c_void {
+        let mut _contents : *mut libc::c_void = ::std::ptr::null_mut();
+        libc::posix_memalign(&mut _contents, alignment, size);
+
+        _contents
+    }
+
+    pub unsafe fn make_executable(addr: *mut libc::c_void, size: libc::size_t) {
+        libc::mprotect(addr, size, libc::PROT_EXEC | libc::PROT_READ | libc::PROT_WRITE);
+    }
 }
 
 const PAGE_SIZE: usize = 4096;
@@ -21,17 +55,15 @@ impl JitMemory {
         let contents : *mut u8;
         unsafe {
             let size = num_pages * PAGE_SIZE;
-            let mut _contents : *mut libc::c_void = _aligned_malloc(size, PAGE_SIZE);
-            let mut _previous_protect : u32 = 0;
-            kernel32::VirtualProtect(_contents as *mut std::os::raw::c_void, size as u64, 
-                winapi::winnt::PAGE_EXECUTE_READWRITE, ((&mut _previous_protect) as *mut u32) as u32);
+            let mut _contents : *mut libc::c_void = memory::aligned_malloc(size, PAGE_SIZE);
+            memory::make_executable(_contents, size);
 
             memset(_contents, 0xc3, size);  // for now, prepopulate with 'RET'
 
             contents = mem::transmute(_contents);
         }
 
-        JitMemory { contents: contents }        
+        JitMemory { contents: contents }
     }
 }
 
